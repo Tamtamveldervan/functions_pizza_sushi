9847dad9d1bbd45078e827c303d63f1500aa081f
Tamtamveldervan <84381450+Tamtamveldervan@users.noreply.github.com>
2021-06-20 12:50:16 +0200

    Add files via upload

---
+++ pizzafunctions.js
@@ -0,0 +1,23 @@
+const makePizza=function(){
+console.log("weeg bloem")
+console.log("meng water, zout en gist met bloem")
+console.log("kneed door elkaar")
+}
+
+
+const makeSushi=function(){
+    console.log("snijd vis")
+    console.log("leg rijst op zeewier")
+    console.log("leg vis op rijst")
+    console.log("rol zeewier op")
+}
+
+makeSushi();
+makeSushi();
+makeSushi();
+
+makePizza();
+makePizza();
+
+
+
